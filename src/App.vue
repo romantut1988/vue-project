9eb560c9df2d60d6f9a049247ed523282403b0ba
Roman <romantut1988@mail.ru>
2024-02-28 00:17:09 +0300

    product search milestone

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,30 @@ import Drawer from './components/Drawer.vue'
 </script>
 
 <template>
-  <Drawer />
+  <!-- <Drawer /> -->
+
   <div class="w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
 
     <div class="p-10">
-      <h2 class="text-3xl font-bold mb-8">Все товары</h2>
+      <div class="flex justify-between items-center">
+        <h2 class="text-3xl font-bold mb-8">Все товары</h2>
+        <div class="flex gap-4">
+          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
+            <option value="">По названию</option>
+            <option value="">По цене (дешевые)</option>
+            <option value="">По цене (дорогме)</option>
+          </select>
+          <div class="relative">
+            <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
+            <input
+              class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
+              type="text"
+              placeholder="Поиск..."
+            />
+          </div>
+        </div>
+      </div>
       <CardList />
     </div>
   </div>
