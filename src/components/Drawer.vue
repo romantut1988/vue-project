5f12d631c745c6907baddf027660271ad4d58b93
Roman <romantut1988@mail.ru>
2024-02-27 23:28:11 +0300

    add basket

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,3 +1,32 @@
+<script setup>
+import DrawerHead from './DrawerHead.vue'
+import CartItemList from './CartItemList.vue'
+</script>
+
 <template>
-  <div></div>
+  <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
+  <div class="bg-white w-96 h-full fixed right-0 top-0 z-20 p-8">
+    <DrawerHead />
+    <CartItemList />
+
+    <div class="flex flex-col gap-4 mt-7">
+      <div class="flex gap-2">
+        <span>Итого:</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>12900 ₽</b>
+      </div>
+
+      <div class="flex gap-2">
+        <span>Налог 5%</span>
+        <div class="flex-1 border-b border-dashed"></div>
+        <b>900 ₽</b>
+      </div>
+      <button
+        disabled=""
+        class="mt-4 transition bg-lime-500 w-full rounded-xl disabled:bg-slate-300 py-3 text-white hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+      >
+        Оформить заказ
+      </button>
+    </div>
+  </div>
 </template>
