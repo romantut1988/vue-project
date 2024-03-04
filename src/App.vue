d39a47923065f2c04a36b9614a1416d457124c8d
Roman <romantut1988@mail.ru>
2024-03-04 23:23:34 +0300

    sorting of goods

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 
 import Header from './components/Header.vue'
@@ -8,14 +8,38 @@ import Drawer from './components/Drawer.vue'
 
 const items = ref([])
 
-onMounted(async () => {
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
+
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
-    const { data } = await axios.get('https://0da4e3fbe4abe8d6.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy
+    }
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+    const { data } = await axios.get(`https://0da4e3fbe4abe8d6.mokky.dev/items`, {
+      params
+    })
     items.value = data
   } catch (err) {
     console.log(err)
   }
-})
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -28,14 +52,20 @@ onMounted(async () => {
       <div class="flex justify-between items-center">
         <h2 class="text-3xl font-bold mb-8">Все товары</h2>
         <div class="flex gap-4">
-          <select class="py-2 px-3 border rounded-md outline-none" name="" id="">
-            <option value="">По названию</option>
-            <option value="">По цене (дешевые)</option>
-            <option value="">По цене (дорогме)</option>
+          <select
+            @change="onChangeSelect"
+            class="py-2 px-3 border rounded-md outline-none"
+            name=""
+            id=""
+          >
+            <option value="name">По названию</option>
+            <option value="price">По цене (дешевые)</option>
+            <option value="-price">По цене (дорогме)</option>
           </select>
           <div class="relative">
             <img class="absolute left-4 top-3" src="/search.svg" alt="search" />
             <input
+              @input="onChangeSearchInput"
               class="border rounded-md py-2 pl-11 pr-4 outline-none focus:border-gray-400"
               type="text"
               placeholder="Поиск..."
